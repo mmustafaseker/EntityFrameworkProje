2f643114b43b4e8e84fb67ad39ea33a99da4e71e
mmustafaseker <mustiseker.06@gmail.com>
2021-03-10 00:43:18 +0300

    WebAPI

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,7 +10,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        internal static string MaintenanceTime = "Sistem bakımda";
-        internal static string ProductListed = "Ürünler listelendi";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
     }
 }
